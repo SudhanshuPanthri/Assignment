072e479923494f8b580a872f326516629500da72
sudhanshu panthri <panthrisudhanshu666@gmail.com>
2024-02-07 01:13:28 +0530

    Register Model

---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment.Models
+{
+    public class RegisterViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "password does not match")]
+        public string confirmPassword { get; set; }
+    }
+}
